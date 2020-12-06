3668375784ce565aa2fe1fa2a5408f91138b1320
Noy Bengio <noytoyota@gmail.com>
2020-12-06 18:13:04 +0200

    trying to deploy

--- api/index.js
+++ api/index.js
@@ -32,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(cors());
 
 app.use(
     cors({
